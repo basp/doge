e37067d98b0cd9f47bb3f592ce329b60f64acf78
Bas Pennings <baspster@gmail.com>
2014-11-11 23:31:42 +0100

    Stuff

--- Doge.Web/Controllers/AppController.cs
+++ Doge.Web/Controllers/AppController.cs
@@ -1,9 +1,13 @@
 ﻿namespace Doge.Web.Controllers
 {
+    using DotLiquid;
     using System.Web.Mvc;
 
     public class AppController : Controller
     {
+        readonly string connectionString =
+            @"Data Source=PREDATOR\SQLEXPRESS;Initial Catalog=sandbox;Integrated Security=SSPI";
+
         readonly QueryParser parser = new QueryParser();
 
         [HttpGet]
@@ -16,6 +20,11 @@
         public ActionResult Test(string q)
         {
             var m = this.parser.Parse(q);
+
+            var t = Template.Parse(Utils.ReadEmbeddedString("Doge.Web.Resources.filtered_events.sql"));
+            var h = Hash.FromAnonymousObject(new { predicates = m.Predicates });
+            var sql = t.Render(h);
+            this.ViewBag.sql = sql;
 
             return this.View();
         }
--- Doge.Web/SearchPredicate.cs
+++ Doge.Web/SearchPredicate.cs
@@ -1,13 +1,23 @@
 ﻿namespace Doge.Web
 {
+    using DotLiquid;
     using Newtonsoft.Json;
 
-    public class SearchPredicate
+    public class SearchPredicate : ILiquidizable
     {
         [JsonProperty("xexp")]
         public string XExp { get; set; }
 
         [JsonProperty("neg")]
         public bool Neg { get; set; }
+
+        public object ToLiquid()
+        {
+            return new
+            {
+                xexp = this.XExp,
+                neg = this.Neg
+            };
+        }
     }
 }
